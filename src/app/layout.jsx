85af58cfe3eab10519120a359c9858199b98afed
MHShohag911 <83244649+MHShohag911@users.noreply.github.com>
2026-09-07 22:41:35 +0600

    Complete Banner and Change Bangla Font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/layouts/Navbar";
 import Footer from "@/components/layouts/Footer";
@@ -8,6 +9,11 @@ const poppins = Poppins(
     weight: ["100", "200", "400", "500", "600", "800"]
   }
 )
+
+export const fontBangla = localFont({
+  src: "../fonts/mayaboti-normal.ttf",
+  // weight: ""
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -24,7 +30,7 @@ export default function RootLayout({ children }) {
         <header className="py-2 md:w-11/12 mx-auto">
           <Navbar></Navbar>
         </header>
-        <main className="py-2 md:w-11/12 mx-auto">
+        <main className="py-2 md:w-11/12 mx-auto min-h-[calc(100vh-302px)]">
           {children}
         </main>
         <footer>
